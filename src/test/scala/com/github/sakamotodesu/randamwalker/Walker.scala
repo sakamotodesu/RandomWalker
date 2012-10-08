d1b43f46dade2dfca7c61baef86f05ccdfc52902
sakamotodesu <sakamoto_desugananika@i.softbank.jp>
2012-10-08 09:58:27 +0900

    add test case

--- src/test/scala/com/github/sakamotodesu/randamwalker/Walker.scala
+++ src/test/scala/com/github/sakamotodesu/randamwalker/Walker.scala
@@ -3,36 +3,59 @@ import org.specs2.mutable._
 import com.github.sakamotodesu.randomwalker.RandomWalker._
 
 class WalkerSpec extends Specification {
-  "The 'Walker'" should {
-    " can move to (1,0)(0,1) " in {
+  "The 'Walker' after start" should {
+    " be movable in (1,0)(0,1) " in {
       val fool = new FoolWalker(Map(3,3),3)
       val list = fool.movable(new Way(List(Point(0,0)),0))
       list must beEqualTo(List(Point(1,0),Point(0,1)))
     }
-    " can move to (1,2)(0,1) " in {
+    " be movable in (1,2)(0,1) " in {
       val fool = new FoolWalker(Map(3,3),3)
       val list = fool.movable(new Way(List(Point(0,2)),0))
       list must beEqualTo(List(Point(1,2),Point(0,1)))
     }
-    " can move to (1,0)(2,1) " in {
+    " be movable in (1,0)(2,1) " in {
       val fool = new FoolWalker(Map(3,3),3)
       val list = fool.movable(new Way(List(Point(2,0)),0))
       list must beEqualTo(List(Point(1,0),Point(2,1)))
     }
-    " can move to (1,2)(2,1) " in {
+    " be movable in (1,2)(2,1) " in {
       val fool = new FoolWalker(Map(3,3),3)
       val list = fool.movable(new Way(List(Point(2,2)),0))
       list must beEqualTo(List(Point(1,2),Point(2,1)))
     }
-    " can move to (1,1)(0,0)(1,1) " in {
+    " be movable in (1,1)(0,0)(1,1) " in {
       val fool = new FoolWalker(Map(3,3),3)
       val list = fool.movable(new Way(List(Point(1,0)),0))
       list must beEqualTo(List(Point(2,0),Point(0,0),Point(1,1)))
     }
-    " can move to (2,1)(0,1)(1,0)(1,2) " in {
+    " be movable in (2,1)(0,1)(1,0)(1,2) " in {
       val fool = new FoolWalker(Map(3,3),3)
       val list = fool.movable(new Way(List(Point(1,1)),0))
       list must beEqualTo(List(Point(2,1),Point(0,1),Point(1,0),Point(1,2)))
+    }
+  }
+
+  "The 'Walker' walked a few steps" should {
+    " be movable in (1,1)(0,2) " in {
+      val fool = new FoolWalker(Map(3,3),3)
+      val list = fool.movable(new Way(List(Point(0,1),Point(0,0)),0))
+      list must beEqualTo(List(Point(1,1),Point(0,2)))
+    }
+    " be movable in (1,2) " in {
+      val fool = new FoolWalker(Map(3,3),3)
+      val list = fool.movable(new Way(List(Point(0,2),Point(0,1),Point(0,0)),0))
+      list must beEqualTo(List(Point(1,2)))
+    }
+    " be movable in (2,0)(0,0) " in {
+      val fool = new FoolWalker(Map(3,3),3)
+      val list = fool.movable(new Way(List(Point(1,0),Point(1,1),Point(0,1),Point(0,0)),0))
+      list must beEqualTo(List(Point(2,0),Point(0,0)))
+    }
+    " be movable in Nil " in {
+      val fool = new FoolWalker(Map(3,3),3)
+      val list = fool.movable(new Way(List(Point(0,0),Point(1,0),Point(1,1),Point(0,1),Point(0,0)),0))
+      list must beEqualTo(Nil)
     }
   }
 }
