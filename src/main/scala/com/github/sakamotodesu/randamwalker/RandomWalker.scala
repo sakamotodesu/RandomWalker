2d86af96a8628f108a62b71cb23ca0a9c9e2395a
Minoru Sakamoto <p2020640@gmail.com>
2012-10-11 14:18:41 +0900

    refactoring "Way"

--- src/main/scala/com/github/sakamotodesu/randamwalker/RandomWalker.scala
+++ src/main/scala/com/github/sakamotodesu/randamwalker/RandomWalker.scala
@@ -7,6 +7,8 @@ object RandomWalker {
     val map = Map(8,8)
     val walker = new FoolWalker(map,15)
     val way = walker.start(Point(4,0))
+    println("steps:" + way.length)
+    println("turn:" + turnCount(way))
     println(way)
     println(VisibleMap(map,way))
   }
@@ -15,7 +17,35 @@ object RandomWalker {
     def contains(p:Point) = if(0 <= p.x  && p.x <= x - 1 && 0 <= p.y && p.y <= y - 1) true else false
   }
 
-  def VisibleMap(map:Map,way:Way) =  {
+  case class Point(x:Int,y:Int) {
+    def up = Point(x+1,y)
+    def down = Point(x-1,y)
+    def left = Point(x,y-1)
+    def right = Point(x,y+1)
+  }
+
+  abstract class Walker (map:Map,maxTurn:Int){
+    def walk(w:List[Point], p:Point) =  p::w
+    def movable(w:List[Point]) = List(w.head.up, w.head.down, w.head.left, w.head.right).filter(map contains _).filter( n => !(passed(n,w))).filter( n =>  if (w.length < 2 ) true else (isTurn(n,w.tail.head) + turnCount(w) <= maxTurn)) 
+    def think(w:List[Point],next:List[Point]):Point
+    def start(p:Point) = {
+      def walking(w:List[Point]):List[Point] = movable(w) match {
+        case Nil => w
+        case l => walking(walk(w,think(w,l)))
+      }
+      walking(List(p))
+    }
+  }
+
+  class FoolWalker(map:Map,maxTurn:Int) extends Walker(map,maxTurn){
+    def think(w:List[Point],next:List[Point]) =  next(new Random() nextInt(next length))
+  }
+
+  def passed(p:Point,way:List[Point]) = ((way indexOfSlice List(p,way.head)) != -1) || ((way indexOfSlice List(way.head,p)) != -1)
+  def isTurn(next:Point,prev:Point) =  if((next.x - prev.x ).abs == 2 || (next.y - prev.y).abs == 2) 0 else 1 
+  def turnCount(way:List[Point]) = if(way.length < 2 ) 0 else ( way zip  way.tail.tail).map( n => isTurn(n._1, n._2)).sum
+
+  def VisibleMap(map:Map,way:List[Point]) =  {
     def expand(n:Int) = 2 * n + 1
     def path(n:Int) = expand(n)/2
     val vm = Array.ofDim[String](expand(map.x), expand(map.y))
@@ -25,44 +55,5 @@ object RandomWalker {
     vm(expand(way.last.x))(expand(way.last.y)) = "S"
     way zip way.tail map( n => vm(path(n._1.x)+path(n._2.x)+1)(path(n._1.y)+path(n._2.y)+1) = "+")
     vm.map(_.fold("")((z,n)=> z + " " + n + " ")).fold("")((z,n)=>z+n+"\n") 
-  }
-
-  case class Point(x:Int,y:Int) extends {
-    def up = Point(x+1,y)
-    def down = Point(x-1,y)
-    def left = Point(x,y-1)
-    def right = Point(x,y+1)
-  }
-
-  case class Way(way:List[Point], turn:Int){ //TODO: sepalate List,turn
-    def add(p:Point, t:Int) =  Way(p::way, t)
-    def stepCount = way.size - 1
-    def head = way.head
-    def last = way.last
-    def tail = Way(way.tail,turn)
-    def before = way.tail.head // TODO: safe
-    def contains(p:Point) = way contains p
-    def zip(that:Way) = this.way zip that.way
-    def passed(p:Point) = ((way indexOfSlice List(p,head)) != -1) || ((way indexOfSlice List(head,p)) != -1)
-    def isTurn(p:Point) = if(stepCount < 2) 0 else if((p.x - before.x ).abs == 2 || (p.y - before.y).abs == 2) 0 else 1 
-    def path = if (stepCount < 2 ) (head,Nil) else (head,before)
-    override def toString = "Step:" + stepCount + "\nturn:" + turn +"\nWay:" + way.toString
-  }
-
-  abstract class Walker (map:Map,maxTurn:Int){
-    def walk(w:Way, p:Point) =  w.add(p,w.turn + (w isTurn p))
-    def movable(w:Way) = List(w.head.up, w.head.down, w.head.left, w.head.right).filter(map contains _).filter( n => !(w passed n)).filter( n => (w isTurn n) + w.turn <= maxTurn) 
-    def think(w:Way,next:List[Point]):Point
-    def start(p:Point) = {
-      def walking(w:Way):Way = movable(w) match {
-        case Nil => w
-        case l => walking(walk(w,think(w,l)))
-      }
-      walking(Way(List(p),0))
-    }
-  }
-
-  class FoolWalker(map:Map,maxTurn:Int) extends Walker(map,maxTurn){
-    def think(w:Way,next:List[Point]) =  next(new Random() nextInt(next length))
   }
 }
--- src/test/scala/com/github/sakamotodesu/randamwalker/Way.scala
+++ src/test/scala/com/github/sakamotodesu/randamwalker/Way.scala
@@ -4,97 +4,31 @@ import com.github.sakamotodesu.randomwalker.RandomWalker._
 
 class WaySpec extends Specification {
 
-  "The 'Way.stepCount'" should {
-    "be 0 if start" in {
-      val way = Way(List(Point(0,0)),0)
-      way.stepCount must beEqualTo(0)
-    }
-    "be 1 if 1 step" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      way1.stepCount must beEqualTo(1)
-    }
-    "be 5 if 5 steps" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      val way2 = way1.add(Point(0,2),0)
-      val way3 = way2.add(Point(0,3),0)
-      val way4 = way3.add(Point(0,4),0)
-      val way5 = way4.add(Point(0,5),0)
-      way5.stepCount must beEqualTo(5)
-    }
-  }
  
-  "The 'Way.passed'" should {
+  "The 'passed'" should {
     "return false" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      val way2 = way1.add(Point(0,2),0)
-      (way2 passed Point(0,3)) must beFalse
+      val way = List(Point(0,2),Point(0,1),Point(0,0))
+      passed (Point(0,3),way) must beFalse
     }
     "return true" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      (way1 passed Point(0,0)) must beTrue
+      val way = List(Point(0,1),Point(0,0))
+      passed (Point(0,0),way) must beTrue
     }
     "return true" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      val way2 = way1.add(Point(1,1),0)
-      val way3 = way2.add(Point(1,0),0)
-      val way4 = way3.add(Point(0,0),0)
-      (way4 passed Point(0,1)) must beTrue
-    }
-    "return true" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      val way2 = way1.add(Point(1,1),0)
-      val way3 = way2.add(Point(1,0),0)
-      val way4 = way3.add(Point(0,0),0)
-      (way4 passed Point(1,0)) must beTrue
-    }
-    "return false" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      val way2 = way1.add(Point(1,1),0)
-      val way3 = way2.add(Point(1,0),0)
-      val way4 = way3.add(Point(0,0),0)
-      val way5 = way4.add(Point(0,1),0)
-      (way5 passed Point(0,2)) must beFalse
+      val way = List(Point(0,0),Point(1,0),Point(1,1),Point(0,1),Point(0,0))
+      passed (Point(0,1),way) must beTrue
     }
   }
-  "The 'Way.isTurn'" should {
+
+  "The 'isTurn'" should {
     "rutern 0" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      (way1 isTurn Point(0,2)) must beEqualTo(0)
+      isTurn(Point(0,2),Point(0,0)) must beEqualTo(0)
     }
     "rutern 0" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(1,0),0)
-      (way1 isTurn Point(2,0)) must beEqualTo(0)
-    }
-    "rutern 0" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      (way1 isTurn Point(1,1)) must beEqualTo(0)
-    }
-    "rutern 0" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(1,0),0)
-      (way1 isTurn Point(1,1)) must beEqualTo(0)
+      isTurn(Point(2,0),Point(0,0)) must beEqualTo(0)
     }
     "rutern 1" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(1,0),0)
-      val way2 = way1.add(Point(2,0),0)
-      (way2 isTurn Point(2,1)) must beEqualTo(1)
-    }
-    "rutern 1" in {
-      val way = Way(List(Point(0,0)),0)
-      val way1 = way.add(Point(0,1),0)
-      val way2 = way1.add(Point(1,1),1)
-      (way2 isTurn Point(1,2)) must beEqualTo(1)
+      isTurn(Point(1,1),Point(0,0)) must beEqualTo(1)
     }
   }
 }
