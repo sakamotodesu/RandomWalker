6f7e5aa31f29239cf017d848eb01e1de4fca5f22
Minoru Sakamoto <p2020640@gmail.com>
2012-10-24 16:09:21 +0900

    use typesafe stack

---
+++ project/RandomwalkerBuild.scala
@@ -0,0 +1,20 @@
+import sbt._
+import sbt.Keys._
+
+object RandomwalkerBuild extends Build {
+
+  lazy val randomwalker = Project(
+    id = "randomwalker",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "RandomWalker",
+      organization := "com.github.sakamotodesu.randomwalker",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.9.2",
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+      libraryDependencies ++= Seq("com.typesafe.akka" % "akka-actor" % "2.0.1",
+                                   "org.specs2" %% "specs2" % "latest.integration" % "test"),
+      scalacOptions ++= Seq("-deprecation", "-Xmigration")
+    )
+  )
+}
